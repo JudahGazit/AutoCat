c583e315ae77db0e9ce4831f12b4d7c64212a3f7
JudahGazit <gazitrotem@gmail.com>
2022-02-26 12:04:02 +0200

    simplificating binning

--- cat_optimize/cat_optimizer.py
+++ cat_optimize/cat_optimizer.py
@@ -1,5 +1,5 @@
-import keras
-import keras.layers
+# import keras
+# import keras.layers
 import networkx as nx
 import pandas as pd
 import scipy.stats
@@ -22,10 +22,11 @@ def cramers_V(var1, var2):
 
 
 class CatOptimizer:
-    def __init__(self, cat_discover=None, min_items_in_cat=0.005, min_categories_to_handle=5, default_cat_name='ELSE'):
+    def __init__(self, cat_discover=None, min_items_in_cat=0.005, min_categories_to_handle=5, default_cat_name='ELSE', min_categories_for_bins=10):
         self.cat_discover = cat_discover or CatDiscover()
         self.min_items_in_cat = min_items_in_cat
         self.min_categories_to_handle = min_categories_to_handle
+        self.min_categories_for_bins = min_categories_for_bins
         self.default_cat_name = default_cat_name
         self.category_mapping = {}
         self.category_kind = {}
@@ -41,9 +42,6 @@ class CatOptimizer:
 
     def _corr_of_categories(self, categories: pd.Series, target: pd.Series, is_classification):
         if is_classification:
-            # corr = {c: cramers_V((categories == c).astype(int), target.astype(int)) for c in
-            #         categories.unique()}
-            # return pd.Series(corr)
             ctab = pd.crosstab(categories, target, normalize='index')
             points = ctab.values
             g = nx.minimum_spanning_tree(nx.from_numpy_array(scipy.spatial.distance.cdist(points, points)))
@@ -62,15 +60,14 @@ class CatOptimizer:
             if len(value_counts) > self.min_categories_to_handle and len(convert_to_else) > 3:
                 effect_on_target = self._mean_target_per_category(data, column, target, is_classification)
                 effect_on_target = effect_on_target.loc[convert_to_else]
-                _, p = scipy.stats.shapiro(effect_on_target.sample(min(len(effect_on_target), 30)))
-                if p < 0.05 and len(convert_to_else) > 10:
+                if len(convert_to_else) > self.min_categories_for_bins:
                     new_categories = self.__split_into_several_bins(column, convert_to_else, effect_on_target)
                 else:
                     new_categories = [category for category in self.category_mapping[column] if
                                       category not in convert_to_else] + [self.default_cat_name]
                 self.category_mapping[column] = pd.Index(new_categories)
 
-    def __split_into_several_bins(self, column, convert_to_else, effect_on_target, n_bins=1):
+    def __split_into_several_bins(self, column, convert_to_else, effect_on_target, n_bins=3):
         if n_bins > 1:
             clusters = pd.qcut(effect_on_target, n_bins, labels=range(n_bins))
             self.small_categories_mapping[column] = dict(zip(effect_on_target.index, clusters))
@@ -92,18 +89,11 @@ class CatOptimizer:
 
     def _find_and_sort_categories(self, data, target, is_classification):
         for column in self.category_mapping:
-            # mean_target = self._mean_target_per_category(data, column, target, is_classification)
             self.category_kind[column] = 'ordinal'
             corr = self._sort_category_by_correlation(column, data, target, is_classification)
             if abs(corr) < 0.05:
                 self.category_kind[column] = 'one_hot'
-                self._build_one_hot_encoder(data, column, target, is_classification)
-            # if len(mean_target) <= 10 or scipy.stats.shapiro(mean_target.sample(min(len(mean_target), 30)))[1] < 0.05:
-            #     self.category_kind[column] = 'ordinal'
-            #     self._sort_category_by_correlation(column, data, target, is_classification)
-            # else:
-            #     self.category_kind[column] = 'one_hot'
-            #     self._build_one_hot_encoder(data, column, target)
+                self._build_one_hot_encoder(data, column)
         return data
 
     def _mean_target_per_category(self, data, column, target, is_classification):
@@ -124,25 +114,12 @@ class CatOptimizer:
             return cramers_V(data[column].cat.codes, target.astype(int))
         return data[column].cat.codes.corr(target)
 
-    def _build_one_hot_encoder(self, data, column, target, is_classification):
+    def _build_one_hot_encoder(self, data, column):
         num_categories = len(data[column].cat.categories)
-        loss = 'categorical_crossentropy' if is_classification else 'mse'
-        if num_categories > 16 and False:
-            model = keras.models.Sequential()
-            model.add(keras.layers.Input(1))
-            model.add(keras.layers.Embedding(num_categories, 16))
-            model.add(keras.layers.Flatten())
-            model.add(keras.layers.Dense(target.nunique() if is_classification else 1,
-                                         activation='softmax' if is_classification else 'linear'))
-            model.compile(optimizer='adam', loss=loss, metrics=['accuracy' if is_classification else 'mse'])
-            intermediate_layer_model = keras.models.Model(inputs=model.input, outputs=model.layers[0].output)
-            model.fit(data[column].cat.codes, np.eye(target.nunique())[target.values.astype(int)], 32, 2, True)
-            embedding = intermediate_layer_model.predict(np.arange(num_categories)).squeeze()
-        else:
-            embedding = np.eye(num_categories)[:, 1:]
+        embedding = np.eye(num_categories)[:, 1:]
         self.onehot_embeddings[column] = embedding
 
-    def _convert_categories_to_numeric(self, data, column, target=None):
+    def _convert_categories_to_numeric(self, data, column):
         data[column] = data[column].cat.codes
         return data
 
@@ -161,17 +138,13 @@ class CatOptimizer:
 
     def fit_transform(self, data: pd.DataFrame, target: pd.Series):
         data = data.copy()
-        # is_classification = False
         is_classification = target.dtype in (np.int64, np.int, np.int32) or target.apply(float.is_integer).all()
         categorical_columns = self.cat_discover.discover_categories(data)
-        # if target.dtype in (np.int64, np.int, np.int32) or target.apply(float.is_integer).all():
-        #     target = data[data.corrwith(target).abs().sort_values().index[-1]].rename('target')
         data = self._convert_to_categorical(data, categorical_columns)
         self._find_small_categories(data, target, is_classification)
         data = self._convert_missing_categories(data)
         data = self._find_and_sort_categories(data, target, is_classification)
         data = self._convert_by_kind(data)
-        print(data.head())
         return data
 
     def transform(self, data: pd.DataFrame):
