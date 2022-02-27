2a84069806f290b3415f0c02ec34969f3ea25aaa
JudahGazit <gazitrotem@gmail.com>
2022-02-27 10:38:34 +0200

    adding documentation for cat_optimizer.py

--- cat_optimize/cat_optimizer.py
+++ cat_optimize/cat_optimizer.py
@@ -1,33 +1,53 @@
-# import keras
-# import keras.layers
 import networkx as nx
+import numpy as np
 import pandas as pd
 import scipy.stats
-import sklearn
-from matplotlib import pyplot as plt
-import numpy as np
-from sklearn.cluster import DBSCAN
-from sklearn.mixture import GaussianMixture
-import seaborn as sns
 
 from cat_optimize.cat_discover import CatDiscover
 
 
 def cramers_V(var1, var2):
-    crosstab = np.array(pd.crosstab(var1, var2, rownames=None, colnames=None))  # Cross table building
-    stat = scipy.stats.chi2_contingency(crosstab)[0]  # Keeping of the test statistic of the Chi2 test
-    obs = np.sum(crosstab)  # Number of observations
-    mini = min(crosstab.shape) - 1  # Take the minimum value between the columns and the rows of the cross table
+    """
+    Calculate Cramer's V (or Cramer's phi) over two categorical arrays.
+    Based upon the Chi Squared statistic.
+    https://en.wikipedia.org/wiki/Cram%C3%A9r%27s_V
+    """
+    crosstab = np.array(pd.crosstab(var1, var2, rownames=None, colnames=None))
+    stat = scipy.stats.chi2_contingency(crosstab)[0]
+    obs = np.sum(crosstab)
+    mini = min(crosstab.shape) - 1
     return np.sqrt(stat / (obs * mini))
 
 
 class CatOptimizer:
-    def __init__(self, cat_discover=None, min_items_in_cat=0.005, min_categories_to_handle=5, default_cat_name='ELSE', min_categories_for_bins=10):
+    """
+    AutoCat's main optimizer. Automatically detects and embeds categorical features in a given dataset.
+
+    Each categorical feature passes the following pipeline:
+        - Detect small categories
+        - Bin small categories into a single default category / multiple default categories using the target variable
+        - Sort categories by affect on target variable
+        - Convert to One Hot encoding if there is no sign of ordinal meaning to the categorical feature
+
+    Parameters:
+        :param cat_discover: Categorical features finder. defaults to CatDiscover.
+        :param min_items_in_cat: Minimal number (or fraction) of rows to be in each category. Otherwise, categories are
+        converted to the default category / binned together into multiple default categories. Irrelevant if the number of
+        categories is smaller than `min_categories_to_handle`.
+        :param min_categories_to_handle: Minimal number of categories to start dropping unpopular categories.
+        :param default_cat_name: Default label to use in case a category is neglected. Default is `ELSE`.
+        :param min_categories_for_bins: Minimal number of small categories to start binning them into default categories.
+        Otherwise, a single default category is used.
+        :param n_categorical_bins: Number of bins to use when binning small categories together.
+    """
+    def __init__(self, cat_discover=None, min_items_in_cat=0.005, min_categories_to_handle=5, default_cat_name='ELSE',
+                 min_categories_for_bins=10, n_categorical_bins=3):
         self.cat_discover = cat_discover or CatDiscover()
         self.min_items_in_cat = min_items_in_cat
         self.min_categories_to_handle = min_categories_to_handle
         self.min_categories_for_bins = min_categories_for_bins
         self.default_cat_name = default_cat_name
+        self.n_categorical_bins = n_categorical_bins
         self.category_mapping = {}
         self.category_kind = {}
         self.small_categories_mapping = {}
@@ -40,13 +60,16 @@ class CatOptimizer:
             self.category_mapping[column] = category_column.cat.categories
         return data
 
+    def _sort_categories_by_target_classses(self, categories, target):
+        ctab = pd.crosstab(categories, target, normalize='index')
+        graph = nx.minimum_spanning_tree(nx.from_numpy_array(scipy.spatial.distance.cdist(ctab.values, ctab.values)))
+        cat_sort = list(nx.bfs_tree(graph, min(graph, key=lambda k: graph.degree(k))))
+        return cat_sort
+
     def _corr_of_categories(self, categories: pd.Series, target: pd.Series, is_classification):
         if is_classification:
-            ctab = pd.crosstab(categories, target, normalize='index')
-            points = ctab.values
-            g = nx.minimum_spanning_tree(nx.from_numpy_array(scipy.spatial.distance.cdist(points, points)))
-            g_sort = list(nx.bfs_tree(g, min(g, key=lambda k: g.degree(k))))
-            return pd.Series(np.arange(len(g_sort)), index=categories.cat.categories[g_sort])
+            cat_sort = self._sort_categories_by_target_classses(categories, target)
+            return pd.Series(np.arange(len(cat_sort)), index=categories.cat.categories[cat_sort])
         else:
             one_hot = pd.get_dummies(categories)
             return one_hot.corrwith(target)
@@ -60,16 +83,16 @@ class CatOptimizer:
             if len(value_counts) > self.min_categories_to_handle and len(convert_to_else) > 3:
                 effect_on_target = self._mean_target_per_category(data, column, target, is_classification)
                 effect_on_target = effect_on_target.loc[convert_to_else]
-                if len(convert_to_else) > self.min_categories_for_bins:
+                if len(convert_to_else) > self.min_categories_for_bins and not is_classification:
                     new_categories = self.__split_into_several_bins(column, convert_to_else, effect_on_target)
                 else:
                     new_categories = [category for category in self.category_mapping[column] if
                                       category not in convert_to_else] + [self.default_cat_name]
                 self.category_mapping[column] = pd.Index(new_categories)
 
-    def __split_into_several_bins(self, column, convert_to_else, effect_on_target, n_bins=3):
-        if n_bins > 1:
-            clusters = pd.qcut(effect_on_target, n_bins, labels=range(n_bins))
+    def __split_into_several_bins(self, column, convert_to_else, effect_on_target):
+        if self.n_categorical_bins > 1:
+            clusters = pd.qcut(effect_on_target, self.n_categorical_bins, labels=range(self.n_categorical_bins))
             self.small_categories_mapping[column] = dict(zip(effect_on_target.index, clusters))
             new_categories = [category for category in self.category_mapping[column] if category not in convert_to_else] + \
                              [f'{self.default_cat_name}_{i}' for i in np.unique(clusters)]
@@ -136,9 +159,12 @@ class CatOptimizer:
             data = convertors[kind](data, column)
         return data
 
+    def _is_classification(self, target):
+        return target.dtype in (np.int64, np.int, np.int32) or target.apply(float.is_integer).all()
+
     def fit_transform(self, data: pd.DataFrame, target: pd.Series):
         data = data.copy()
-        is_classification = target.dtype in (np.int64, np.int, np.int32) or target.apply(float.is_integer).all()
+        is_classification = self._is_classification(target)
         categorical_columns = self.cat_discover.discover_categories(data)
         data = self._convert_to_categorical(data, categorical_columns)
         self._find_small_categories(data, target, is_classification)
