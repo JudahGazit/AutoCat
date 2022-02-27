6563e86f796f42ae0a9923b1c3900aa75f533396
JudahGazit <gazitrotem@gmail.com>
2022-02-27 10:04:08 +0200

    adding one_hot to naive_cat.py

--- cat_optimize/transformers/naive_cat.py
+++ cat_optimize/transformers/naive_cat.py
@@ -1,24 +1,53 @@
+import numpy as np
 import pandas as pd
 
 
 class NaiveCategories:
-    def __init__(self):
+    """
+    Naive transformer of categories into ordinal / sparse space.
+    Used for comparison of AutoCat to naive transformations
+
+    Parameters:
+        kind - {'ordinal', 'one_hot'} - kind of transformer
+
+    """
+    def __init__(self, kind='ordinal'):
+        self.kind = kind
         self.category_mapping = {}
 
     def fit_transform(self, data: pd.DataFrame, columns: list):
         data = data.copy()
         for column in columns:
-            category_column = data[column].astype('category')
-            data[column] = category_column.cat.codes
-            self.category_mapping[column] = category_column.cat.categories
+            if self.kind == 'ordinal':
+                data = self._to_ordinal(data, column)
+            elif self.kind == 'one_hot':
+                data = self._to_one_hot(data, column)
+        return data
+
+    def _to_ordinal(self, data, column):
+        category_column = data[column].astype('category')
+        data[column] = category_column.cat.codes
+        self.category_mapping[column] = category_column.cat.categories
+        return data
+
+    def _to_one_hot(self, data, column):
+        categories = data[column].unique()
+        categories = pd.DataFrame(np.eye(len(categories)), index=pd.Series(categories, name=column),
+                             columns=[f'{column}={category}' for category in categories])
+        categories = categories.reset_index()
+        self.category_mapping[column] = categories
+        data = data.merge(categories, on=column).drop(columns=[column])
         return data
 
     def transform(self, data: pd.DataFrame):
         data = data.copy()
         for column, categories in self.category_mapping.items():
-            category_column = data[column].astype('category')
-            category_column = category_column.cat.set_categories(categories)
-            data[column] = category_column.cat.codes
+            if self.kind == 'ordinal':
+                category_column = data[column].astype('category')
+                category_column = category_column.cat.set_categories(categories)
+                data[column] = category_column.cat.codes
+            elif self.kind == 'one_hot':
+                data = data.merge(categories, on=column).drop(columns=[column])
         return data
 
     def reverse(self, data: pd.DataFrame):
