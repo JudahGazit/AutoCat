3f1743e6d65ed9dce0f1e65c6f845c7b7f701e96
JudahGazit <gazitrotem@gmail.com>
2022-02-27 10:35:50 +0200

    adding documentation for cat_discover.py

--- cat_optimize/cat_discover.py
+++ cat_optimize/cat_discover.py
@@ -2,5 +2,8 @@ import pandas as pd
 
 
 class CatDiscover:
+    """
+    Simple categorical features discoverer. Detects every column that is either `string` or `categorical`.
+    """
     def discover_categories(self, data: pd.DataFrame):
         return data.dtypes[(data.dtypes == 'object') | (data.dtypes == 'category')].index.tolist()
